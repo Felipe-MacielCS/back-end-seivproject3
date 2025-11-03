4d62aacad6deabe4f10f6bd8cae86d9b04a2ec05
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-02 22:07:29 -0600

    add goal controller

---
+++ app/models/goal.model.js
@@ -0,0 +1,39 @@
+export default (sequelize, DataTypes) => {
+  const Goal = sequelize.define("goal", {
+    goalID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    target: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    metric: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    athleteID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    exerciseID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  return Goal;
+};
