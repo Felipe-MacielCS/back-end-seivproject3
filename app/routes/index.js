0b430d29ebabd8f2612c5c3adb1670af6cf23729
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-03 13:41:54 -0600

    update index routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,24 @@
 import { Router } from "express";
-
-import AuthRoutes from "./auth.routes.js";
-import UserRoutes from "./user.routes.js";
-import TutorialRoutes from "./tutorial.routes.js";
-import LessonRoutes from "./athlete.routes.js";
-
+import User from "./user.routes.js";
+import Athlete from "./athlete.routes.js";
+import Coach from "./coach.routes.js";
+import Sport from "./sport.routes.js";
+import Goal from "./goal.routes.js";
+import Result from "./result.routes.js";
+import Exercise from "./exercise.routes.js";
+import ExercisePool from "./exercisepool.routes.js";
+import ExercisePlan from "./exerciseplan.routes.js";
 
 const router = Router();
 
-router.use("/", AuthRoutes);
-router.use("/users", UserRoutes);
-router.use("/tutorials", TutorialRoutes);
-router.use("/tutorials", LessonRoutes);
+router.use("/users", User);
+router.use("/athletes", Athlete);
+router.use("/coaches", Coach);
+router.use("/sports", Sport);
+router.use("/goals", Goal);
+router.use("/results", Result);
+router.use("/exercises", Exercise);
+router.use("/exercisepools", ExercisePool);
+router.use("/exerciseplans", ExercisePlan);
 
 export default router;
