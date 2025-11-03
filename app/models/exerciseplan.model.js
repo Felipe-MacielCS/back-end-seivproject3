d1a66650c15a495005e45d3947c2daa8f95080bf
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-03 12:43:10 -0600

    add exercise plan model

---
+++ app/models/exerciseplan.model.js
@@ -0,0 +1,31 @@
+export default (sequelize, DataTypes) => {
+  const ExercisePlan = sequelize.define("exerciseplan", {
+    planID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reps: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    repetitions: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    coachID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  return ExercisePlan;
+};
