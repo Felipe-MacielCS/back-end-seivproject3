8a7ea2ea7d962cce47a562c041b6188e7bb2d062
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-03 14:21:41 -0600

    update indexes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,10 @@
 import { Router } from "express";
-import User from "./user.routes.js";
-import Athlete from "./athlete.routes.js";
-import Coach from "./coach.routes.js";
-import Sport from "./sport.routes.js";
-import Goal from "./goal.routes.js";
-import Result from "./result.routes.js";
-import Exercise from "./exercise.routes.js";
-import ExercisePool from "./exercisepool.routes.js";
-import ExercisePlan from "./exerciseplan.routes.js";
 
 const router = Router();
 
-router.use("/users", User);
-router.use("/athletes", Athlete);
-router.use("/coaches", Coach);
-router.use("/sports", Sport);
-router.use("/goals", Goal);
-router.use("/results", Result);
-router.use("/exercises", Exercise);
-router.use("/exercisepools", ExercisePool);
-router.use("/exerciseplans", ExercisePlan);
+
+import CoachRoutes from "./coach.routes.js";
+
+router.use("/coaches", CoachRoutes);
 
 export default router;
