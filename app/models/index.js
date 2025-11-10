4d174ffd76cf2643b4d5c962011625ffd319359b
rikilyn <r.maldonado@eagles.oc.edu>
2025-11-09 23:49:56 -0600

    index update

--- app/models/index.js
+++ app/models/index.js
@@ -3,8 +3,6 @@ import { Sequelize } from "sequelize";
 import sequelize from "../config/sequelizeInstance.js";
 
 // Import only the User model
-import User from "./user.model.js";
-import Goal from "./goal.model.js";
 import Result from "./result.model.js";
 
 // Define the db object
@@ -14,16 +12,8 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // Assign models
-db.user = User;
-db.goal = Goal;
+
 db.result = Result;
-
-// Initialize models
-db.result = Result(sequelize, Sequelize.DataTypes);
-
-// Goal → Result (one-to-many)
-db.goal.hasMany(db.result, { foreignKey: "goalID" });
-db.result.belongsTo(db.goal, { foreignKey: "goalID" });
 
 // Export db
 export default db;
