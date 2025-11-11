42760c2bff50235ddc0b26b993c2502af1570b74
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-10 20:59:15 -0600

    update index

--- app/models/index.js
+++ app/models/index.js
@@ -15,12 +15,12 @@ db.sequelize = sequelize;
 db.user = User;
 db.session = Session;
 db.athlete = Athlete;
-db.Coach = Coach;
+db.coach = Coach;
 
 db.user.hasOne(db.athlete, { foreignKey: "userID", onDelete: "CASCADE" });
-db.User.hasOne(db.Coach, { foreignKey: "userID", onDelete: "CASCADE" });
+db.user.hasOne(db.coach, { foreignKey: "userID", onDelete: "CASCADE" });
 
 db.athlete.belongsTo(db.user, { foreignKey: "userID" });
-db.Coach.belongsTo(db.User, { foreignKey: "userID" });
+db.coach.belongsTo(db.user, { foreignKey: "userID" });
 
 export default db;
