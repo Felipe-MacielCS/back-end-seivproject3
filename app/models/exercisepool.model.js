38d671ed10cd5b8600b22f0ab90486abfde1fd33
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-12 23:09:50 -0600

    update model

--- app/models/exercisepool.model.js
+++ app/models/exercisepool.model.js
@@ -1,5 +1,9 @@
-export default (sequelize, DataTypes) => {
-  const ExercisePool = sequelize.define("exercisepool", {
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelizeInstance.js";
+
+const ExercisePool = sequelize.define(
+  "exercisepool",
+  {
     exerciseID: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -14,9 +18,11 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
-  }, {
+  },
+  {
+    tableName: "exercisepools",
     timestamps: false,
-  });
+  }
+);
 
-  return ExercisePool;
-};
+export default ExercisePool;
