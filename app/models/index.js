b62375d7a6b6f54a798b4f2595bb0bde7880015d
Joao Lima Santos <j.limasantos@eagles.oc.edu>
2025-11-12 23:14:53 -0600

    add exercise pool files

--- app/models/index.js
+++ app/models/index.js
@@ -8,23 +8,24 @@ import Session from "./session.model.js";
 import Coach from "./coach.model.js";
 import Goal from "./goal.model.js";
 import ExercisePlan from "./exerciseplan.model.js";
+import ExercisePool from "./exercisepool.model.js";
 
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Assign models
 db.user = User;
 db.session = Session;
 db.athlete = Athlete;
-db.Coach = Coach;
+db.coach = Coach;
 db.goal = Goal;
 db.exerciseplan = ExercisePlan;
+db.exercisepool = ExercisePool;
 
 db.user.hasOne(db.athlete, { foreignKey: "userID", onDelete: "CASCADE" });
-db.User.hasOne(db.Coach, { foreignKey: "userID", onDelete: "CASCADE" });
+db.user.hasOne(db.coach, { foreignKey: "userID", onDelete: "CASCADE" });
 
 db.athlete.belongsTo(db.user, { foreignKey: "userID" });
-db.Coach.belongsTo(db.User, { foreignKey: "userID" });
+db.coach.belongsTo(db.user, { foreignKey: "userID" });
 
 export default db;
